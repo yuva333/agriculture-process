b1be4b5374d7ad90e609d03583f250b22f5ef5a8
Madhavan-19 <madhavans194@gmail.com>
2025-09-19 16:36:31 +0530

    gh page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/agriculture/"
+  base: "/agriculture-process/"
+
+
+  
 })
